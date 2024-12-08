92322574fa564492cd37ca4ce5e8cbeee67232db
fadrianosilva <f.adriano.silva@outlook.com>
2024-12-08 15:57:54 -0300

    Ajustes na string de conexão e Criação da migration EntidadeMedido

--- obj/Debug/net9.0/SisMed.AssemblyInfo.cs
+++ obj/Debug/net9.0/SisMed.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SisMed")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a519bafe9417aa5f7997f641b55ade04d6a3ebe1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db204431b0b280a34c67d45d9d6e52eab0cbb1a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SisMed")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SisMed")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
